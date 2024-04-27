5ffd4f113f46fcec006cf74c71b2bff65bf5fe7b
redundant4u <physiogel@pusan.ac.kr>
2024-04-27 12:20:57 +0900

    fix: k8s aws ecr secret 스크립트 수정

--- k8s/aws-ecr-secret.sh
+++ k8s/aws-ecr-secret.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-kubectl create secret docker-registry ${SECRET_NAME} \
+kubectl create secret -n ${NAMESPACE} docker-registry ${SECRET_NAME} \
     --docker-server=https://${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com \
     --docker-username=AWS \
     --docker-password="${TOKEN}" \
